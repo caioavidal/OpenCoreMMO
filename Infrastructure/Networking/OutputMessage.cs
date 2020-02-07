eee43f9ff996393ee568656df9a23cf44bea3320
Caio <caio.avidal@gmail.com>
2020-02-07 02:03:13 -0300

    Implemented outputmessage on login

---
+++ Infrastructure/Networking/OutputMessage.cs
@@ -0,0 +1,81 @@
+using System;
+
+public class OutputMessage
+{
+    public byte[] Buffer { get; private set; } = new byte[24590];
+    public int HeaderLength { get; private set; }
+    public int Length { get; private set; } = 0;
+
+    private int _position;
+
+    public OutputMessage(int headerLength)
+    {
+        HeaderLength = headerLength;
+        _position = HeaderLength;
+    }
+
+    public void AddString(string value) => AddBytes(System.Text.Encoding.UTF8.GetBytes(value));
+    public void AddUInt32(uint value) => AddBytes(BitConverter.GetBytes(value));
+    public void AddUInt16(ushort value) => AddBytes(BitConverter.GetBytes(value));
+
+    public void AddByte(byte b) => AddBytes(new[] { b });
+
+    public void AddPaddingBytes(int count) => AddBytes(0x33, count);
+
+    public void AddHeader(bool addChecksum)
+    {
+        if (addChecksum)
+        {
+            var adlerChecksum = Checksum.AdlerChecksum(Buffer, HeaderLength, Length);
+            AddChecksumToHeader(BitConverter.GetBytes(adlerChecksum));
+        }
+        AddLengthToHeader();
+
+    }
+    private byte[] GetLengthBytes() =>  BitConverter.GetBytes((ushort)Length);
+
+    private void AddLengthToHeader()
+    {
+        var length = GetLengthBytes();
+
+        for (int i = 0; i < length.Length; i++)
+        {
+            WriteByte(length[i], i);
+        }
+    }
+
+    private void AddChecksumToHeader(byte[] checksum)
+    {
+        var cIndex = 0;
+        for (int i = 2; i < HeaderLength; i++)
+        {
+            WriteByte(checksum[cIndex++], i);
+        }
+    }
+
+    private void AddBytes(byte[] bytes)
+    {
+        foreach (var b in bytes)
+        {
+            WriteByte(b);
+        }
+    }
+    private void AddBytes(byte b, int times)
+    {
+        for (int i = 0; i < times; i++)
+        {
+            WriteByte(b);
+        }
+    }
+
+    private void WriteByte(byte b, int position)
+    {
+        Length++;
+        Buffer[position] = b;
+    }
+    private void WriteByte(byte b)
+    {
+        Length++;
+        Buffer[_position++] = b;
+    }
+}
