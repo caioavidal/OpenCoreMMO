f3dc90c3bba416b2edbb690214d4b5b0ee2bb160
Caio <caio.avidal@gmail.com>
2020-04-06 17:12:35 -0300

    Changed root folder structure

---
+++ src/NeoServer.OTB.Tests/ReadOnlyArrayTest.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using NeoServer.OTB.DataStructures;
+using NeoServer.OTB.Enums;
+using NeoServer.OTB.Parsers;
+using Xunit;
+
+namespace NeoServer.OTB.Tests
+{
+    public class ReadOnlyArrayTest
+    {
+
+
+
+        [Fact]
+        public void WrapCollection_NullItems_Throws()
+        {
+            Assert.Throws<NullReferenceException>(() => ReadOnlyArray<int>.WrapCollection(null));
+        }
+
+        [Fact]
+        public void WrapCollection_NotNull_ReturnsInstance()
+        {
+            var instance = ReadOnlyArray<int>.WrapCollection(new int[0]);
+            Assert.NotNull(instance);
+            Assert.IsType<ReadOnlyArray<int>>(instance);
+        }
+
+        [Fact]
+        public void GetByIndex_ReturnsValue()
+        {
+            var instance = ReadOnlyArray<int>.WrapCollection(new int[4] { 4, 7, 5, 1 });
+
+            Assert.Equal(5, instance[2]);
+            Assert.Equal(7, instance[1]);
+            Assert.Equal(4, instance[0]);
+            Assert.Throws<IndexOutOfRangeException>(() => instance[8]);
+            Assert.Throws<IndexOutOfRangeException>(() => instance[-5]);
+        }
+        [Fact]
+        public void GetCount_ReturnsArrayLength()
+        {
+            var instance = ReadOnlyArray<int>.WrapCollection(new int[4] { 4, 7, 5, 1 });
+
+            Assert.Equal(4, instance.Count);
+
+        }
+
+        [Fact]
+        public void GetEnumerator_ReturnsGenericEnumerator()
+        {
+            var data = new int[4] { 4, 7, 5, 1 };
+            var instance = ReadOnlyArray<int>.WrapCollection(data);
+
+            Assert.NotNull(instance.GetEnumerator());
+        }
+
+   
+    }
+}
---
+++ src/NeoServer.Server.Events/PlayerAddedOnMapEventHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using NeoServer.Enums.Creatures.Enums;
+using NeoServer.Game.Contracts;
+using NeoServer.Game.Contracts.Creatures;
+using NeoServer.Game.Events;
+using NeoServer.Networking.Packets.Outgoing;
+using NeoServer.Server.Contracts.Network;
+using NeoServer.Server.Model.Players.Contracts;
+using NeoServer.Server.Schedulers;
+using NeoServer.Server.Schedulers.Contracts;
+
+namespace NeoServer.Server.Events
+{
+    public class PlayerAddedOnMapEventHandler : IEventHandler<PlayerAddedOnMapEvent>
+    {
+        private readonly IMap map;
+        private readonly Game game;
+
+        public PlayerAddedOnMapEventHandler(IMap map, Game game)
+        {
+            this.map = map;
+            this.game = game;
+        }
+        public void Execute(PlayerAddedOnMapEvent evt)
+        {
+            var player = evt.Player;
+
+            var outgoingPackets = new Queue<IOutgoingPacket>();
+
+            foreach (var spectatorId in map.GetCreaturesAtPositionZone(player.Location))
+            {
+                var isSpectator = !(player.CreatureId == spectatorId);
+                if (!isSpectator)
+                {
+                    SendPacketsToPlayer(player, outgoingPackets);
+                }
+                else
+                {
+                    var spectator = game.CreatureInstances[spectatorId] as IPlayer;
+                    SendPacketsToSpectator(spectator, player, outgoingPackets);
+                }
+
+                if (game.Connections.TryGetValue(spectatorId, out IConnection connection))
+                {
+                    connection.Send(outgoingPackets, isSpectator);
+                }
+            }
+
+
+        }
+
+        private void SendPacketsToSpectator(IPlayer playerToSend, ICreature creatureAdded, Queue<IOutgoingPacket> outgoingPackets)
+        {
+            //spectator.add
+            outgoingPackets.Enqueue(new AddAtStackPositionPacket((IPlayer)creatureAdded));
+            outgoingPackets.Enqueue(new AddCreaturePacket(playerToSend, creatureAdded));
+            outgoingPackets.Enqueue(new MagicEffectPacket(creatureAdded.Location, EffectT.BubbleBlue));
+
+        }
+
+        private void SendPacketsToPlayer(IPlayer player, Queue<IOutgoingPacket> outgoingPackets)
+        {
+            outgoingPackets.Enqueue(new SelfAppearPacket(player));
+            outgoingPackets.Enqueue(new MapDescriptionPacket(player, map));
+            outgoingPackets.Enqueue(new MagicEffectPacket(player.Location, EffectT.BubbleBlue));
+            outgoingPackets.Enqueue(new PlayerInventoryPacket(player.Inventory));
+            outgoingPackets.Enqueue(new PlayerStatusPacket(player));
+            outgoingPackets.Enqueue(new PlayerSkillsPacket(player));
+
+            outgoingPackets.Enqueue(new WorldLightPacket(game.LightLevel, game.LightColor));
+
+            outgoingPackets.Enqueue(new CreatureLightPacket(player));
+
+            outgoingPackets.Enqueue(new PlayerConditionsPacket());
+
+
+        }
+    }
+}
