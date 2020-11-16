a8ccb8214b0a97039ed95d7bd61e4fdfb6813980
Caio <caio.avidal@gmail.com>
2020-11-16 17:04:25 -0300

    small fix on attack event

--- src/NeoServer.Server.Events/Combat/CreatureChangedAttackTargetEventHandler.cs
+++ src/NeoServer.Server.Events/Combat/CreatureChangedAttackTargetEventHandler.cs
@@ -1,5 +1,6 @@
 ﻿using NeoServer.Game.Contracts.Creatures;
 using NeoServer.Server.Tasks;
+using System;
 
 namespace NeoServer.Server.Events.Combat
 {
@@ -19,18 +20,18 @@ namespace NeoServer.Server.Events.Combat
                 return;
             }
 
-            Attack(actor);
-
+            var result = Attack(actor);
+            var attackSpeed = result ? actor.BaseAttackSpeed : 300;
             actor.AttackEvent = game.Scheduler.AddEvent(new SchedulerEvent((int)actor.BaseAttackSpeed, () => Attack(actor)));
         }
-        private void Attack(ICombatActor actor)
+        private bool Attack(ICombatActor actor)
         {
+            var result = false;
             if (actor.Attacking)
             {
                 if (game.CreatureManager.TryGetCreature(actor.AutoAttackTargetId, out var creature) && creature is ICombatActor enemy)
                 {
-                    actor.Attack(enemy);
-                   // MoveAroundEnemy(actor);
+                    result = actor.Attack(enemy);
                 }
             }
             else
@@ -47,6 +48,7 @@ namespace NeoServer.Server.Events.Combat
                 actor.AttackEvent = 0;
                 Execute(actor, 0, 0);
             }
+            return result;
         }
 
         private void MoveAroundEnemy(ICombatActor actor)
