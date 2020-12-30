c72dcc3d0919a5ea318b3deb63fdff7d8302c75c
Caio <caio.avidal@gmail.com>
2020-12-30 12:55:14 -0300

    add itemmodelparser

---
+++ src/NeoServer.Data/Parsers/ItemModelParser.cs
@@ -0,0 +1,65 @@
+﻿//using NeoServer.Data.Model;
+//using NeoServer.Game.Common;
+//using NeoServer.Game.Common.Location.Structs;
+//using NeoServer.Game.Contracts.Items;
+//using NeoServer.Game.Contracts.Items.Types;
+//using NeoServer.Server.Model.Players;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace NeoServer.Data.Parsers
+//{
+//    public class ItemModelParser
+//    {
+//        public static PlayerItemModel ToModel(IItem item)
+//        {
+//            var itemModel = new PlayerItemModel()
+//            {
+//                Itemtype = (short)item.Metadata.TypeId,
+//                //ServerId = item.Metadata.TypeId,
+//                Amount = item is ICumulative cumulative ? cumulative.Amount : 1
+//            };
+
+//            if (item is IContainer container)
+//            {
+//                var items = new List<PlayerItemModel>();
+//                foreach (var i in container.Items)
+//                {
+//                    items.Add(ToModel(i));
+//                }
+//                itemModel.Items = items;
+//            }
+//            return itemModel;
+//        }
+//        public static IItem BuildContainer(List<ItemModel> items, int index, Location location, IContainer container, IItemFactory itemFactory)
+//        {
+//            if (items == null || items.Count == index)
+//            {
+//                return container;
+//            }
+
+//            var itemModel = items[index];
+
+//            var item = itemFactory.Create(itemModel.ServerId, location, new Dictionary<ItemAttribute, IConvertible>()
+//                        {
+//                            {ItemAttribute.Count, itemModel.Amount }
+//                        });
+
+//            if (item is IContainer childrenContainer)
+//            {
+//                childrenContainer.SetParent(container);
+//                container.AddItem(BuildContainer(itemModel.Items?.Reverse().ToList(), 0, location, childrenContainer, itemFactory));
+//            }
+//            else
+//            {
+//                container.AddItem(item);
+
+//            }
+//            return BuildContainer(items, ++index, location, container, itemFactory);
+//        }
+
+//    }
+//}
