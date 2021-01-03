00480f23cd911ca6e3923d80f596efd2b7002a05
Caio <caio.avidal@gmail.com>
2021-01-03 08:52:58 -0300

    fix attack effect

--- src/NeoServer.Server.Events/Combat/CreatureAttackEventHandler.cs
+++ src/NeoServer.Server.Events/Combat/CreatureAttackEventHandler.cs
@@ -54,7 +54,11 @@ namespace NeoServer.Server.Events.Combat
 
         private static void SendEffect(CombatAttackType attack, IConnection connection, Location location)
         {
+            attack.EffectT = attack.EffectT == 0 ? EffectT.None : attack.EffectT;
             var effect = attack.EffectT == EffectT.None ? DamageEffectParser.Parse(attack.DamageType) : attack.EffectT;
+
+            if (attack.EffectT == EffectT.None && attack.DamageType == NeoServer.Game.Common.Item.DamageType.Melee) return;
+
             connection.OutgoingPackets.Enqueue(new MagicEffectPacket(location, effect));
         }
 
