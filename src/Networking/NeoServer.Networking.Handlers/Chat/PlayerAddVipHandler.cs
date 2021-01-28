b3db671d5e95e8cd8fe119101a1fac13c1ce6515
Caio <caio.avidal@gmail.com>
2021-01-28 08:55:24 -0300

    add flag to not permit player be added to vip lists

--- src/Networking/NeoServer.Networking.Handlers/Chat/PlayerAddVipHandler.cs
+++ src/Networking/NeoServer.Networking.Handlers/Chat/PlayerAddVipHandler.cs
@@ -1,6 +1,7 @@
 ﻿using NeoServer.Data.Interfaces;
 using NeoServer.Game.Contracts.Chats;
 using NeoServer.Game.DataStore;
+using NeoServer.Loaders.Interfaces;
 using NeoServer.Networking.Packets.Incoming;
 using NeoServer.Networking.Packets.Incoming.Chat;
 using NeoServer.Networking.Packets.Outgoing;
@@ -8,6 +9,7 @@ using NeoServer.Server.Contracts.Network;
 using NeoServer.Server.Model.Players;
 using NeoServer.Server.Model.Players.Contracts;
 using NeoServer.Server.Tasks;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace NeoServer.Server.Handlers.Player
@@ -16,10 +18,12 @@ namespace NeoServer.Server.Handlers.Player
     {
         private readonly Game game;
         private readonly IAccountRepository accountRepository;
-        public PlayerAddVipHandler(Game game, IAccountRepository accountRepository)
+        private readonly IEnumerable<IPlayerLoader> playerLoaders;
+        public PlayerAddVipHandler(Game game, IAccountRepository accountRepository, IEnumerable<IPlayerLoader> playerLoaders)
         {
             this.game = game;
             this.accountRepository = accountRepository;
+            this.playerLoaders = playerLoaders;
         }
         public override async void HandlerMessage(IReadOnlyNetworkMessage message, IConnection connection)
         {
@@ -28,16 +32,25 @@ namespace NeoServer.Server.Handlers.Player
             if (addVipPacket.Name?.Length > 20) return;
             if (!game.CreatureManager.TryGetPlayer(connection.CreatureId, out var player)) return;
 
-            var vipPlayerModel = await accountRepository.GetPlayer(addVipPacket.Name);
 
-            if (vipPlayerModel is null)
+            if (!game.CreatureManager.TryGetPlayer(addVipPacket.Name, out var vipPlayer))
+            {
+                var playerRecord = await accountRepository.GetPlayer(addVipPacket.Name);
+                if (playerLoaders.FirstOrDefault(x => x.IsApplicable(playerRecord)) is not IPlayerLoader playerLoader) return;
+
+                vipPlayer = playerLoader.Load(playerRecord);
+            }
+            
+
+            if (vipPlayer is null)
             {
                 connection.Send(new TextMessagePacket("A player with this name does not exist.", TextMessageOutgoingType.Small));
                 return;
             }
+
             //todo: check if player can be added to vip list
 
-            game.Dispatcher.AddEvent(new Event(() => player.AddToVip((uint)vipPlayerModel.PlayerId, vipPlayerModel.Name)));
+            game.Dispatcher.AddEvent(new Event(() => player.AddToVip(vipPlayer)));
         }
     }
 }
