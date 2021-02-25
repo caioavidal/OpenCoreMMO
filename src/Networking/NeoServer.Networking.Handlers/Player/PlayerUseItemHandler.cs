c05374765c098e1183bfcace04750a919bdf73fd
Caio <caio.avidal@gmail.com>
2021-02-25 16:45:50 -0300

    change PlayerUseItemCommand to be inject as singleton

--- src/Networking/NeoServer.Networking.Handlers/Player/PlayerUseItemHandler.cs
+++ src/Networking/NeoServer.Networking.Handlers/Player/PlayerUseItemHandler.cs
@@ -9,16 +9,18 @@ namespace NeoServer.Server.Handlers.Player
     public class PlayerUseItemHandler : PacketHandler
     {
         private readonly IGameServer game;
-        public PlayerUseItemHandler(IGameServer game)
+        private readonly PlayerUseItemCommand playerUseItemCommand;
+        public PlayerUseItemHandler(IGameServer game, PlayerUseItemCommand playerUseItemCommand)
         {
             this.game = game;
+            this.playerUseItemCommand = playerUseItemCommand;
         }
         public override void HandlerMessage(IReadOnlyNetworkMessage message, IConnection connection)
         {
             var useItemPacket = new UseItemPacket(message);
             if (game.CreatureManager.TryGetPlayer(connection.CreatureId, out var player))
             {
-                game.Dispatcher.AddEvent(new Event(2000, new PlayerUseItemCommand(player, game, useItemPacket).Execute)); //todo create a const for 2000 expiration time
+                game.Dispatcher.AddEvent(new Event(2000, ()=> playerUseItemCommand.Execute(player, useItemPacket))); //todo create a const for 2000 expiration time
             }
         }
     }
