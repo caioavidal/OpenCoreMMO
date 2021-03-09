1dcee2101fd76c37aeb08831c4c3e5f83dff4a0d
Caio <caio.avidal@gmail.com>
2021-03-09 16:06:53 -0300

    Add interval control to npc job

---
+++ src/Server/NeoServer.Server.Jobs/IntervalControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoServer.Server.Jobs
+{
+     public class IntervalControl
+    {
+        private int interval;
+        private DateTime lastRun;
+
+        public IntervalControl(int interval)
+        {
+            this.interval = interval;
+        }
+
+        public void MarkAsExecuted() => lastRun = DateTime.Now;
+        public bool CanExecuteNow() => DateTime.Now  >= lastRun.AddMilliseconds(interval);
+    }
+}
