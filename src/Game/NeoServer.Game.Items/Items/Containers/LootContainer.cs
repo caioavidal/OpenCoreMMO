324705cc9afcf0ecf40e5b893d613695f181da84
Caio <caio.avidal@gmail.com>
2021-04-15 17:06:15 -0300

    add loot message to channel

--- src/Game/NeoServer.Game.Items/Items/Containers/LootContainer.cs
+++ src/Game/NeoServer.Game.Items/Items/Containers/LootContainer.cs
@@ -2,8 +2,11 @@
 using NeoServer.Game.Common.Location.Structs;
 using NeoServer.Game.Contracts.Creatures;
 using NeoServer.Game.Contracts.Items;
+using NeoServer.Game.Contracts.Items.Types;
+using NeoServer.Game.DataStore;
 using System;
 using System.Linq;
+using System.Text;
 
 namespace NeoServer.Game.Items.Items.Containers
 {
@@ -32,5 +35,38 @@ namespace NeoServer.Game.Items.Items.Containers
         public bool CanBeOpenedBy(IPlayer player) => Allowed(player);
         public bool CanBeMovedBy(IPlayer player) => Allowed(player);
         public void MarkAsLootCreated() => LootCreated = true;
+
+        public override string ToString()
+        {
+            if (LootCreated) return base.ToString();
+            
+            var content = GetStringContent(Loot?.Items);
+            if (string.IsNullOrWhiteSpace(content)) return "nothing";
+
+            return content.Remove(content.Length - 2, 2);
+        }
+
+        private string GetStringContent(ILootItem[] items)
+        {
+            if (Loot is null) return string.Empty;
+            var stringBuilder = new StringBuilder();
+
+            foreach (var item in items)
+            {
+                var itemType = ItemTypeStore.Data.Get(item.ItemId);
+                if (item.Amount > 1) stringBuilder.Append($"{item.Amount} {itemType.Name}");
+                else stringBuilder.Append($"{itemType.Name}");
+
+                stringBuilder.Append(", ");
+
+                if (item.Items?.Any() ?? false)
+                {
+                    stringBuilder.Append(GetStringContent(item.Items));
+                    stringBuilder.Append(", ");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
--- src/Loaders/NeoServer.Loaders/Monsters/Converters/MonsterLootConverter.cs
+++ src/Loaders/NeoServer.Loaders/Monsters/Converters/MonsterLootConverter.cs
@@ -1,6 +1,7 @@
 ﻿using NeoServer.Game.Contracts.Creatures;
 using NeoServer.Game.Creatures;
 using System.Collections.Generic;
+using System.Linq;
 using static NeoServer.Loaders.Monsters.MonsterData;
 
 namespace NeoServer.Loaders.Monsters.Converters
@@ -13,12 +14,23 @@ namespace NeoServer.Loaders.Monsters.Converters
 
             var items = new List<ILootItem>();
 
-            foreach (var item in data.Loot)
+            foreach (var item in Normalize(data.Loot))
             {
                 items.Add(ConvertToLootItem(item));
             }
 
             return new Game.Creatures.Model.Monsters.Loots.Loot(items.ToArray(), null, lootRate);
+        }
+
+        private static List<LootData> Normalize(List<LootData> lootData)
+        {
+            return lootData?.GroupBy(x => x.Id)?.Select(gp => new LootData
+            {
+                Chance = gp.FirstOrDefault().Chance,
+                Id = gp.Key,
+                Countmax = gp.Sum(s => byte.TryParse(s.Countmax, out byte amount) ? amount : 0).ToString(),
+                Items = Normalize(gp.FirstOrDefault().Items)
+            })?.ToList();
         }
 
         private static ILootItem ConvertToLootItem(LootData item)
