37b1fb75bfe7f1793e3882b5001403b4d9b225da
Shelby Timbrook <shelbytimbrook@gmail.com>
2021-08-21 16:14:03 -0400

    The start of a standardized way of applying experience bonuses.

---
+++ src/Game/NeoServer.Game.Creatures/Experience/ExperienceBonusType.cs
@@ -0,0 +1,13 @@
+﻿namespace NeoServer.Game.Creatures.Experience
+{
+    /// <summary>
+    /// When multiple experience bonuses of the same type exist, they will be added together.
+    /// When multiple experience bonuses of different types exist, they will be multiplied together.
+    /// </summary>
+    public enum ExperienceBonusType
+    {
+        Standard = 0,
+        Stamina,
+        GlobalBonus
+    }
+}
