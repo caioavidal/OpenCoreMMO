4945649cc3421f6288dd4e8a1fdc6ce147732e40
Shelby Timbrook <timbrook316@hotmail.com>
2021-08-30 22:19:39 -0400

    Created unit tests checking if using runes grants skills. I suspect it may still happen despite the tests due to event subscribers and other like events.

--- tests/NeoServer.Game.Creatures.Tests/WalkableCreature/PlayerTest.cs
+++ tests/NeoServer.Game.Creatures.Tests/WalkableCreature/PlayerTest.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Moq;
+using NeoServer.Extensions.Runes;
 using NeoServer.Game.Common.Contracts.Creatures;
+using NeoServer.Game.Common.Contracts.Items;
 using NeoServer.Game.Common.Contracts.World;
 using NeoServer.Game.Common.Contracts.World.Tiles;
 using NeoServer.Game.Common.Creatures;
+using NeoServer.Game.Common.Item;
 using NeoServer.Game.Common.Location;
 using NeoServer.Game.Common.Location.Structs;
 using NeoServer.Game.Creatures.Model.Players;
 using NeoServer.Game.DataStore;
+using NeoServer.Game.Items.Items.UsableItems.Runes;
 using NeoServer.Game.Tests;
 using NeoServer.Game.Tests.Helpers;
 using Xunit;
@@ -177,6 +182,125 @@ namespace NeoServer.Game.Creatures.Tests.WalkableCreature
             Assert.True(startedWalkingEvent);
             Assert.Equal(Direction.North, sut.GetNextStep());
             Assert.Equal(Direction.East, sut.GetNextStep());
+        }
+
+        [Fact]
+        public void PlayerDoesNotGainSkillsWhenUsingAnAttackRune()
+        {
+            var player = PlayerTestDataBuilder.BuildPlayer();
+            var targetPlayer = PlayerTestDataBuilder.BuildPlayer();
+
+            var itemAttirbuteListMock = new Mock<IItemAttributeList>();
+
+            itemAttirbuteListMock
+                .Setup(x => x.GetAttribute<bool>(It.IsAny<ItemAttribute>()))
+                .Returns(true);
+
+            itemAttirbuteListMock
+                .Setup(x => x.GetAttributeArray(It.IsAny<string>()))
+                .Returns(new dynamic[] { "0.0", "0.0" });
+
+            var itemTypeMock = new Mock<IItemType>();
+            itemTypeMock
+                .Setup(x => x.Attributes)
+                .Returns(itemAttirbuteListMock.Object);
+
+            var rune = new AttackRune(itemTypeMock.Object, new Location(100, 100, 7), (byte)10);
+
+            var before = new Dictionary<SkillType, byte>()
+            {
+                { SkillType.Axe, player.GetSkillTries(SkillType.Axe) },
+                { SkillType.Club, player.GetSkillTries(SkillType.Club) },
+                { SkillType.Distance, player.GetSkillTries(SkillType.Distance) },
+                { SkillType.Fishing, player.GetSkillTries(SkillType.Fishing) },
+                { SkillType.Fist, player.GetSkillTries(SkillType.Fist) },
+                { SkillType.Level, player.GetSkillTries(SkillType.Level) },
+                { SkillType.Magic, player.GetSkillTries(SkillType.Magic) },
+                { SkillType.Shielding, player.GetSkillTries(SkillType.Shielding) },
+                { SkillType.Speed, player.GetSkillTries(SkillType.Speed) },
+                { SkillType.Sword, player.GetSkillTries(SkillType.Sword) },
+            };
+
+            var result = rune.Use(player, targetPlayer, out var attackType);
+            Assert.True(result);
+
+            var after = new Dictionary<SkillType, byte>()
+            {
+                { SkillType.Axe, player.GetSkillTries(SkillType.Axe) },
+                { SkillType.Club, player.GetSkillTries(SkillType.Club) },
+                { SkillType.Distance, player.GetSkillTries(SkillType.Distance) },
+                { SkillType.Fishing, player.GetSkillTries(SkillType.Fishing) },
+                { SkillType.Fist, player.GetSkillTries(SkillType.Fist) },
+                { SkillType.Level, player.GetSkillTries(SkillType.Level) },
+                { SkillType.Magic, player.GetSkillTries(SkillType.Magic) },
+                { SkillType.Shielding, player.GetSkillTries(SkillType.Shielding) },
+                { SkillType.Speed, player.GetSkillTries(SkillType.Speed) },
+                { SkillType.Sword, player.GetSkillTries(SkillType.Sword) },
+            };
+
+            foreach (var skillType in before.Keys)
+            {
+                Assert.Equal(before[skillType], after[skillType]);
+            }
+        }
+
+        [Fact]
+        public void PlayerDoesNotGainSkillsWhenUsingAHealingRune()
+        {
+            var player = PlayerTestDataBuilder.BuildPlayer();
+            var targetPlayer = PlayerTestDataBuilder.BuildPlayer();
+
+            var itemAttirbuteListMock = new Mock<IItemAttributeList>();
+
+            itemAttirbuteListMock
+                .Setup(x => x.GetAttribute<bool>(It.IsAny<ItemAttribute>()))
+                .Returns(true);
+
+            itemAttirbuteListMock
+                .Setup(x => x.GetAttributeArray(It.IsAny<string>()))
+                .Returns(new dynamic[] { "0.0", "0.0" });
+
+            var itemTypeMock = new Mock<IItemType>();
+            itemTypeMock
+                .Setup(x => x.Attributes)
+                .Returns(itemAttirbuteListMock.Object);
+
+            var rune = new HealingRune(itemTypeMock.Object, new Location(100, 100, 7), new Dictionary<ItemAttribute, IConvertible>());
+
+            var before = new Dictionary<SkillType, byte>()
+            {
+                { SkillType.Axe, player.GetSkillTries(SkillType.Axe) },
+                { SkillType.Club, player.GetSkillTries(SkillType.Club) },
+                { SkillType.Distance, player.GetSkillTries(SkillType.Distance) },
+                { SkillType.Fishing, player.GetSkillTries(SkillType.Fishing) },
+                { SkillType.Fist, player.GetSkillTries(SkillType.Fist) },
+                { SkillType.Level, player.GetSkillTries(SkillType.Level) },
+                { SkillType.Magic, player.GetSkillTries(SkillType.Magic) },
+                { SkillType.Shielding, player.GetSkillTries(SkillType.Shielding) },
+                { SkillType.Speed, player.GetSkillTries(SkillType.Speed) },
+                { SkillType.Sword, player.GetSkillTries(SkillType.Sword) },
+            };
+
+            rune.Use(player, targetPlayer);
+
+            var after = new Dictionary<SkillType, byte>()
+            {
+                { SkillType.Axe, player.GetSkillTries(SkillType.Axe) },
+                { SkillType.Club, player.GetSkillTries(SkillType.Club) },
+                { SkillType.Distance, player.GetSkillTries(SkillType.Distance) },
+                { SkillType.Fishing, player.GetSkillTries(SkillType.Fishing) },
+                { SkillType.Fist, player.GetSkillTries(SkillType.Fist) },
+                { SkillType.Level, player.GetSkillTries(SkillType.Level) },
+                { SkillType.Magic, player.GetSkillTries(SkillType.Magic) },
+                { SkillType.Shielding, player.GetSkillTries(SkillType.Shielding) },
+                { SkillType.Speed, player.GetSkillTries(SkillType.Speed) },
+                { SkillType.Sword, player.GetSkillTries(SkillType.Sword) },
+            };
+
+            foreach (var skillType in before.Keys)
+            {
+                Assert.Equal(before[skillType], after[skillType]);
+            }
         }
     }
 }
