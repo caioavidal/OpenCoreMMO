bd9061a3c71162f0abfd3416a472c3510a136d65
Caio Vidal <caio.avidal@gmail.com>
2021-09-12 20:02:14 -0300

    Add attributes instances to Weapons

--- src/Game/NeoServer.Game.Items/Factories/WeaponFactory.cs
+++ src/Game/NeoServer.Game.Items/Factories/WeaponFactory.cs
@@ -5,6 +5,7 @@ using NeoServer.Game.Common.Contracts.Items;
 using NeoServer.Game.Common.Contracts.Items.Types;
 using NeoServer.Game.Common.Item;
 using NeoServer.Game.Common.Location.Structs;
+using NeoServer.Game.Items.Factories.AttributeFactory;
 using NeoServer.Game.Items.Items.Weapons;
 
 namespace NeoServer.Game.Items.Factories
@@ -12,13 +13,53 @@ namespace NeoServer.Game.Items.Factories
     public class WeaponFactory : IFactory
     {
         public event CreateItem OnItemCreated;
-        
+        private readonly DecayableFactory _decayableFactory;
+        private readonly ProtectionFactory _protectionFactory;
+        private readonly SkillBonusFactory _skillBonusFactory;
+        private readonly TransformableFactory _transformableFactory;
+        private readonly ChargeableFactory _chargeableFactory;
+
+        public WeaponFactory(DecayableFactory decayableFactory, ProtectionFactory protectionFactory, SkillBonusFactory skillBonusFactory, TransformableFactory transformableFactory, ChargeableFactory chargeableFactory)
+        {
+            _decayableFactory = decayableFactory;
+            _protectionFactory = protectionFactory;
+            _skillBonusFactory = skillBonusFactory;
+            _transformableFactory = transformableFactory;
+            _chargeableFactory = chargeableFactory;
+        }
 
         public IItem Create(IItemType itemType, Location location, IDictionary<ItemAttribute, IConvertible> attributes)
         {
-            if (MeleeWeapon.IsApplicable(itemType)) return new MeleeWeapon(itemType, location);
-            if (DistanceWeapon.IsApplicable(itemType)) return new DistanceWeapon(itemType, location);
-            if (MagicWeapon.IsApplicable(itemType)) return new MagicWeapon(itemType, location);
+            var decayable = _decayableFactory.Create(itemType);
+            var skillBonuses = _skillBonusFactory.Create(itemType);
+            var protection = _protectionFactory.Create(itemType);
+            var transformable = _transformableFactory.Create(itemType);
+            var chargeable = _chargeableFactory.Create(itemType);
+
+            if (MeleeWeapon.IsApplicable(itemType)) return new MeleeWeapon(itemType, location)
+            {
+                Decayable = decayable,
+                SkillBonus = skillBonuses,
+                Protection = protection,
+                Transformable = transformable,
+                Chargeable = chargeable
+            };
+            if (DistanceWeapon.IsApplicable(itemType)) return new DistanceWeapon(itemType, location)
+            {
+                Decayable = decayable,
+                SkillBonus = skillBonuses,
+                Protection = protection,
+                Transformable = transformable,
+                Chargeable = chargeable
+            };
+            if (MagicWeapon.IsApplicable(itemType)) return new MagicWeapon(itemType, location)
+            {
+                Decayable = decayable,
+                SkillBonus = skillBonuses,
+                Protection = protection,
+                Transformable = transformable,
+                Chargeable = chargeable
+            };
 
             if (ICumulative.IsApplicable(itemType))
             {
