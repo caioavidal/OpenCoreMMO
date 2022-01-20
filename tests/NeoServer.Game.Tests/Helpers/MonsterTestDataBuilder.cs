1aacb4a8c664704364d4c831e7fb7f12dc4d58e6
Caio Vidal <caio.avidal@gmail.com>
2022-01-20 09:31:45 -0300

    remove unused variable

--- tests/NeoServer.Game.Tests/Helpers/MonsterTestDataBuilder.cs
+++ tests/NeoServer.Game.Tests/Helpers/MonsterTestDataBuilder.cs
@@ -48,7 +48,6 @@ namespace NeoServer.Game.Tests.Helpers
         {
             var map = MapTestDataBuilder.Build(100, 110, 100, 110, 7, 7, addGround: true);
             var pathFinder = new PathFinder(map);
-            var spawnPoint = new SpawnPoint(new Location(105, 105, 7), 60);
 
             var mapTool = new MapTool(map, pathFinder);
 
