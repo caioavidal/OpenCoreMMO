8f4d467b9fa55cce21841302dea62d73bdc05c03
Caio Vidal <caio.avidal@gmail.com>
2022-05-27 20:28:11 -0300

    Add flame strike spell

---
+++ data/extensions/Spells/Attack/FlameStrike.cs
@@ -0,0 +1,15 @@
+﻿using NeoServer.Game.Combat.Attacks;
+using NeoServer.Game.Combat.Spells;
+using NeoServer.Game.Common;
+using NeoServer.Game.Common.Item;
+
+namespace NeoServer.Extensions.Spells.Attack
+{
+    public class FlameStrike: AttackSpell
+    {
+        public override DamageType DamageType => DamageType.Fire;
+        public override CombatAttack CombatAttack =>  new DistanceCombatAttack(Range, ShootType.Fire);
+        public override MinMax Damage => new(5, 100);
+        public override byte Range => 5;
+    }
+}
