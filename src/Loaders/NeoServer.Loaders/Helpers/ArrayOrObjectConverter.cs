3d0c9284ff081c19bc83a8cd8b4040ab52fde795
Caio Vidal <caio.avidal@gmail.com>
2022-07-06 19:33:26 -0300

    Move converter to a class

---
+++ src/Loaders/NeoServer.Loaders/Helpers/ArrayOrObjectConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NeoServer.Loaders.Helpers;
+
+internal class ArrayOrObjectConverter<T> : JsonConverter
+{
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        var token = JToken.Load(reader);
+        return token.Type == JTokenType.Array
+            ? token.ToObject<List<T>>()
+            : new List<T> { token.ToObject<T>() };
+    }
+
+    public override bool CanConvert(Type objectType)
+        => objectType == typeof(List<T>);
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        =>throw new NotImplementedException(); 
+}
--- src/Loaders/NeoServer.Loaders/Spawns/SpawnData.cs
+++ src/Loaders/NeoServer.Loaders/Spawns/SpawnData.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using NeoServer.Loaders.Helpers;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace NeoServer.Loaders.Spawns;
 
@@ -42,19 +41,3 @@ public class SpawnData
     }
 }
 
-class ArrayOrObjectConverter<T> : JsonConverter
-{
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-        var token = JToken.Load(reader);
-        return token.Type == JTokenType.Array
-            ? token.ToObject<List<T>>()
-            : new List<T> { token.ToObject<T>() };
-    }
-
-    public override bool CanConvert(Type objectType)
-        => objectType == typeof(List<T>);
-
-    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        =>throw new NotImplementedException(); 
-}
