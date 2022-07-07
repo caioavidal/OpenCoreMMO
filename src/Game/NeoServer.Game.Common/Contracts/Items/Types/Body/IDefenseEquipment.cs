9a0eab50eb9dff4560700993c91a39eda4934318
Caio Vidal <caio.avidal@gmail.com>
2022-07-07 16:08:29 -0300

    Fix weapon defense

--- src/Game/NeoServer.Game.Common/Contracts/Items/Types/Body/IDefenseEquipment.cs
+++ src/Game/NeoServer.Game.Common/Contracts/Items/Types/Body/IDefenseEquipment.cs
@@ -4,8 +4,8 @@ namespace NeoServer.Game.Common.Contracts.Items.Types.Body;
 
 public interface IDefenseEquipment : IBodyEquipmentEquipment, IEquipment
 {
-    ushort DefenseValue => Metadata.Attributes.HasAttribute(ItemAttribute.WeaponDefendValue)
-        ? Metadata.Attributes.GetAttribute<byte>(ItemAttribute.WeaponDefendValue)
+    ushort DefenseValue => Metadata.Attributes.HasAttribute(ItemAttribute.Defense)
+        ? Metadata.Attributes.GetAttribute<byte>(ItemAttribute.Defense)
         : Metadata.Attributes.GetAttribute<byte>(ItemAttribute.ArmorValue);
 
     ushort ArmorValue => Metadata.Attributes.GetAttribute<byte>(ItemAttribute.ArmorValue);
