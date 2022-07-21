8182bd1624ae8fbc688c8328429cdfd5b09bee41
Marcus Vinicius Santana Silva <lowpoc.developer@gmail.com>
2022-07-21 00:04:35 -0300

    feat: add PlayerChangeCompletedOutFitHandler

---
+++ src/Networking/NeoServer.Networking.Handlers/Player/PlayerChangeCompletedOutFitHandler.cs
@@ -0,0 +1,22 @@
+﻿using NeoServer.Game.Common.Contracts.DataStores;
+using NeoServer.Server.Common.Contracts;
+using NeoServer.Server.Common.Contracts.Network;
+
+namespace NeoServer.Networking.Handlers.Player;
+
+public class PlayerChangeCompletedOutFitHandler : PacketHandler
+{
+    private readonly IGameServer game;
+    private readonly IPlayerOutFitStore _playerOutFitStore;
+
+    public PlayerChangeCompletedOutFitHandler(IGameServer game, IPlayerOutFitStore playerOutFitStore)
+    {
+        this.game = game;
+        _playerOutFitStore = playerOutFitStore;
+    }
+    
+    public override void HandlerMessage(IReadOnlyNetworkMessage message, IConnection connection)
+    {
+        if (!game.CreatureManager.TryGetPlayer(connection.CreatureId, out var player)) return;
+    }
+}
