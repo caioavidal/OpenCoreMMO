9d9bddd7a0624370fecf40d899f75a41bcb4d0f9
Marcus Silva <marcus.silva@xpi.com.br>
2022-11-02 21:15:58 -0300

    feat: Create Container Diagram with c4sharp.

---
+++ src/NeoServer.Architecture/Diagrams/ContainerDiagram.cs
@@ -0,0 +1,46 @@
+﻿using C4Sharp.Diagrams;
+using C4Sharp.Models;
+using C4Sharp.Models.Relationships;
+using NeoServer.Architecture.Structures;
+
+namespace NeoServer.Architecture.Diagrams;
+
+public class ContainerDiagram : DiagramBuildRunner
+{
+    protected override IEnumerable<Structure> Structures() => new Structure[]
+    {
+        People.Customer,
+        Containers.Client,
+        new SoftwareSystemBoundary("c1", "OpenCoreMMO.Networking", Containers.NetworkingServer),
+        new SoftwareSystemBoundary("c2", "OpenCoreMMO.Game",
+            Containers.GameServer,
+            Containers.JobsServer,
+            Containers.LoadServer),
+        new SoftwareSystemBoundary("c3", "OpenCoreMMO.Data",
+            Containers.SqlDatabase,
+            Containers.MemoryDatabase),
+        new SoftwareSystemBoundary("c4", "OpenCoreMMO.SystemFile",
+            Containers.FileSystemToLoader),
+    };
+
+    protected override IEnumerable<Relationship> Relationships() => new[]
+    {
+        (People.Customer > Containers.Client)["Uses", "TCP and HTTPS"][Position.Down],
+        
+        (Containers.Client >= Containers.NetworkingServer)["Uses", "TCP"][Position.Down],
+        (Containers.NetworkingServer > Containers.GameServer)["Uses", "Events"][Position.Up],
+        (Containers.GameServer > Containers.SqlDatabase)["Uses", "Events"][Position.Neighbor],
+        (Containers.GameServer > Containers.MemoryDatabase)["Uses", "Events"][Position.Neighbor],
+        (Containers.JobsServer > Containers.SqlDatabase)["Uses", "TCP"][Position.Neighbor],
+        (Containers.JobsServer > Containers.GameServer)["Uses", "Events"],
+        (Containers.LoadServer > Containers.FileSystemToLoader)["Uses", "I/O"][Position.Up],
+        (Containers.LoadServer > Containers.MemoryDatabase)["Uses"][Position.Up],
+        (Containers.LoadServer > Containers.SqlDatabase)["Uses", "TCP"][Position.Up]
+    };
+
+    protected override string Title => "Container diagram for OpenCoreMMO";
+    protected override DiagramType DiagramType => DiagramType.Container;
+    protected override DiagramLayout FlowVisualization => DiagramLayout.LeftRight;
+    protected override bool ShowLegend  => true;
+    protected override bool LayoutAsSketch => false;
+}
---
+++ src/NeoServer.Architecture/Structures/People.cs
@@ -0,0 +1,15 @@
+﻿using C4Sharp.Models;
+using C4Sharp.Models.Relationships;
+
+namespace NeoServer.Architecture.Structures;
+
+public static class People
+{
+    private static Person? _customer;
+    public static Person Customer => _customer ??= new Person("customer", "Personal")
+    {
+        Description = "A player of the tibia, with personal tibia accounts.",
+        Boundary = Boundary.External,
+        Label = "Player"
+    };
+}
