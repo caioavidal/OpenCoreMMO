4c7c83ae1c7e14ba7d20769605cdca3741cb14c4
Caio Vidal <caio.avidal@gmail.com>
2022-11-15 17:36:07 -0300

    Fix unit test

--- tests/NeoServer.Game.Creatures.Tests/Players/PlayerFollowTests.cs
+++ tests/NeoServer.Game.Creatures.Tests/Players/PlayerFollowTests.cs
@@ -3,7 +3,9 @@ using NeoServer.Game.Common.Combat.Structs;
 using NeoServer.Game.Common.Item;
 using NeoServer.Game.Common.Location.Structs;
 using NeoServer.Game.Tests.Helpers;
+using NeoServer.Game.Tests.Helpers.Map;
 using NeoServer.Game.Tests.Helpers.Player;
+using NeoServer.Game.World.Models.Tiles;
 using Xunit;
 
 namespace NeoServer.Game.Creatures.Tests.Players;
@@ -14,11 +16,17 @@ public class PlayerFollowTests
     public void Player_does_not_follow_dead_creature()
     {
         //arrange
+
+        var map = MapTestDataBuilder.Build(100, 110, 100, 110, 7, 7);
+        
         var sut = PlayerTestDataBuilder.Build(hp: 200);
         var enemy = MonsterTestDataBuilder.Build(1);
         using var monitor = sut.Monitor();
 
         var fpp = new FindPathParams(true, true, true, false, 12, 0, 12, false);
+
+        (map[100, 100, 7] as DynamicTile)?.AddCreature(sut);
+        (map[100, 108, 7] as DynamicTile)?.AddCreature(enemy);
 
         sut.Follow(enemy, fpp);
         enemy.ReceiveAttack(sut, new CombatDamage(200, DamageType.Melee));
