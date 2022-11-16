62d4ed5b0a0e7bc4e514a973f93b12e565202678
Caio Vidal <caio.avidal@gmail.com>
2022-11-16 08:11:03 -0300

    Add unit tests

--- tests/NeoServer.Game.Creatures.Tests/Players/PlayerFollowTests.cs
+++ tests/NeoServer.Game.Creatures.Tests/Players/PlayerFollowTests.cs
@@ -40,4 +40,65 @@ public class PlayerFollowTests
         sut.HasNextStep.Should().BeFalse();
         monitor.Should().Raise(nameof(sut.OnStoppedWalking));
     }
+    
+    [Fact]
+    public void Player_does_emit_stopped_follow_event_if_has_no_further_step()
+    {
+        //arrange
+
+        var map = MapTestDataBuilder.Build(100, 101, 100, 101, 7, 7);
+        
+        var sut = PlayerTestDataBuilder.Build(hp: 200);
+        var enemy = MonsterTestDataBuilder.Build(1);
+        using var monitor = sut.Monitor();
+
+        var fpp = new FindPathParams(true, true, true, false, 12, 0, 12, false);
+
+        (map[100, 100, 7] as DynamicTile)?.AddCreature(sut);
+        (map[100, 101, 7] as DynamicTile)?.AddCreature(enemy);
+
+        sut.Follow(enemy, fpp);
+        enemy.ReceiveAttack(sut, new CombatDamage(200, DamageType.Melee));
+
+        //act
+        sut.Follow(enemy, fpp);
+
+        //assert
+        sut.IsFollowing.Should().BeFalse();
+        sut.Following.Should().BeNull();
+        sut.HasNextStep.Should().BeFalse();
+        monitor.Should().NotRaise(nameof(sut.OnStoppedWalking));
+    }
+    
+    [Fact]
+    public void Player_does_not_follow_if_creature_disappears()
+    {
+        //arrange
+
+        var map = MapTestDataBuilder.Build(100, 110, 100, 110, 7, 7);
+        
+        var sut = PlayerTestDataBuilder.Build(hp: 200);
+        var enemy = MonsterTestDataBuilder.Build(1000);
+        using var monitor = sut.Monitor();
+
+        var fpp = new FindPathParams(true, true, true, false, 12, 0, 12, false);
+
+        (map[100, 100, 7] as DynamicTile)?.AddCreature(sut);
+        (map[100, 109, 7] as DynamicTile)?.AddCreature(enemy);
+
+        sut.Follow(enemy, fpp);
+        enemy.ReceiveAttack(sut, new CombatDamage(200, DamageType.Melee));
+        
+        (map[100, 109, 7] as DynamicTile)?.RemoveCreature(enemy, out _);
+        (map[100, 110, 7] as DynamicTile)?.AddCreature(enemy);
+
+        //act
+        sut.Follow(enemy, fpp);
+
+        //assert
+        sut.IsFollowing.Should().BeFalse();
+        sut.Following.Should().BeNull();
+        sut.HasNextStep.Should().BeFalse();
+        monitor.Should().Raise(nameof(sut.OnStoppedWalking));
+    }
 }
