8073a77be1b7929502a05cf253e9c0985e4d628f
Caio Vidal <caio.avidal@gmail.com>
2022-12-22 18:40:49 -0300

    Fix text break line

---
+++ src/Game/NeoServer.Game.Common/Helpers/StringBuilderExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace NeoServer.Game.Common.Helpers;
+
+public static class StringBuilderExtension
+{
+    public static StringBuilder AppendNewLine(this StringBuilder stringBuilder) => stringBuilder.Append('\n');
+    public static StringBuilder AppendNewLine(this StringBuilder stringBuilder, string value) => stringBuilder.Append($"{value}\n");
+}
