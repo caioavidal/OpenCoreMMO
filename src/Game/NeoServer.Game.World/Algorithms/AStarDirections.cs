9cabadf82af9171875e0432526f157412884e997
Marcus.Silva <marcus.silva@xpi.com.br>
2023-01-20 23:51:22 -0300

    refactor(Astar): extract get direction on logic.

---
+++ src/Game/NeoServer.Game.World/Algorithms/AStarDirections.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using NeoServer.Game.Common.Location;
+using NeoServer.Game.Common.Location.Structs;
+
+namespace NeoServer.Game.World.Algorithms;
+
+public class AStarDirections
+{
+    public Direction[] GetAll(AStarNode node, Location startPos, Location endPos)
+    {
+        var dirList = new List<Direction>();
+        var prevPos = new Location(endPos.X, endPos.Y, endPos.Z);
+        node = node.Parent;
+
+        while (node is not null)
+        {
+            var pos = new Location((ushort)node.X, (ushort)node.Y, startPos.Z);
+
+            var direction = Next(prevPos, pos);
+            dirList.Insert(0, direction);
+
+            prevPos = pos;
+            node = node.Parent;
+        }
+
+        return dirList.ToArray();
+    }
+    
+    private Direction Next(Location prevPos, Location pos)
+    {
+        var dx = pos.X - prevPos.X;
+        var dy = pos.Y - prevPos.Y;
+
+        switch (dx)
+        {
+            case 1 when dy == 1:
+                return Direction.NorthWest;
+            case 1 when dy == -1:
+                return Direction.SouthWest;
+            case 1:
+                return Direction.West;
+            case -1 when dy == 1:
+                return Direction.NorthEast;
+            case -1 when dy == -1:
+                return Direction.SouthEast;
+            case -1:
+                return Direction.East;
+            default:
+            {
+                switch (dy)
+                {
+                    case 1:
+                        return Direction.North;
+                    case -1:
+                        return Direction.South;
+                }
+                break;
+            }
+        }
+        
+        return Direction.None;
+    }
+}
