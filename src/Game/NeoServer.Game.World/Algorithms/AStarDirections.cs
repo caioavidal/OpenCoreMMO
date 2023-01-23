81fb059641c3e98713c5f90463b92536610819b0
Caio Vidal <caio.avidal@gmail.com>
2023-01-23 15:42:58 -0300

    fix sightclear and improve AStarDirections.cs

--- src/Game/NeoServer.Game.World/Algorithms/AStarDirections.cs
+++ src/Game/NeoServer.Game.World/Algorithms/AStarDirections.cs
@@ -6,9 +6,10 @@ namespace NeoServer.Game.World.Algorithms;
 
 internal static class AStarDirections
 {
+    private static readonly List<Direction> DirList = new();
     public static Direction[] GetAll(AStarNode node, Location startPos, Location endPos)
     {
-        var dirList = new List<Direction>();
+        DirList.Clear();
         var prevPos = new Location(endPos.X, endPos.Y, endPos.Z);
         node = node.Parent;
 
@@ -17,13 +18,13 @@ internal static class AStarDirections
             var pos = new Location((ushort)node.X, (ushort)node.Y, startPos.Z);
 
             var direction = Next(prevPos, pos);
-            dirList.Insert(0, direction);
+            DirList.Insert(0, direction);
 
             prevPos = pos;
             node = node.Parent;
         }
 
-        return dirList.ToArray();
+        return DirList.ToArray();
     }
     
     private static Direction Next(Location prevPos, Location pos)
