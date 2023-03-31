d18ce6bf24e34cfc3383660d25f166147c18d5a3
MarcusViniciusS <lowpoc.developer@gmail.com>
2023-03-30 21:31:42 -0300

    feat(KickPlayerCommand): add new spell for kick player from game.

---
+++ data/extensions/Spells/Commands/KickPlayerCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NeoServer.Game.Combat.Spells;
+using NeoServer.Game.Common;
+using NeoServer.Game.Common.Contracts.Creatures;
+using NeoServer.Server.Common.Contracts;
+using NeoServer.Server.Helpers;
+
+namespace NeoServer.Extensions.Spells.Commands;
+
+public class KickPlayerCommand : CommandSpell
+{
+    public override bool OnCast(ICombatActor actor, string words, out InvalidOperation error)
+    {
+        var commands = words.Split("/kick");
+
+        if (string.IsNullOrWhiteSpace(commands[1]))
+        {
+            error = InvalidOperation.NotPossible;
+            return false;
+        }
+
+        var ctx  = IoC.GetInstance<IGameCreatureManager>();
+        
+        Console.WriteLine(commands[1]);
+        
+        if (!ctx.TryGetPlayer(commands[1], out var player))
+        {
+            error = InvalidOperation.NotPossible;
+            return false;
+        }
+
+        if (player is null || player.CreatureId == actor.CreatureId)
+        {
+            error = InvalidOperation.NotPossible;
+            return false;
+        }
+        
+        ctx.RemovePlayer(player);
+        error = InvalidOperation.None;
+        return true;
+    }
+}
