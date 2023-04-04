a19f2e316eab1f1dbc59d69de3d3623a8c5d0617
MarcusViniciusS <lowpoc.developer@gmail.com>
2023-04-04 19:07:33 -0300

    feat(commandSpell): possible goto another player.

--- data/extensions/Spells/Commands/GoToCommand.cs
+++ data/extensions/Spells/Commands/GoToCommand.cs
@@ -1,6 +1,9 @@
-﻿using NeoServer.Game.Combat.Spells;
+﻿using System;
+using NeoServer.Game.Combat.Spells;
 using NeoServer.Game.Common;
 using NeoServer.Game.Common.Contracts.Creatures;
+using NeoServer.Server.Common.Contracts;
+using NeoServer.Server.Helpers;
 
 namespace NeoServer.Extensions.Spells.Commands;
 
@@ -10,15 +13,26 @@ public class GoToCommand : CommandSpell
     {
         error = InvalidOperation.NotPossible;
         if (Params?.Length == 0) return false;
-
-        if (Params?.Length == 3)
+        
+        if (Params.Length == 1)
         {
-            ushort.TryParse(Params[0].ToString(), out var x);
-            ushort.TryParse(Params[1].ToString(), out var y);
-            byte.TryParse(Params[2].ToString(), out var z);
-
-            actor.TeleportTo(x, y, z);
+            var creatureManager = IoC.GetInstance<IGameCreatureManager>();
+            creatureManager.TryGetPlayer(Params[0].ToString(), out var target);
+            
+            if (target is null || target.CreatureId == actor.CreatureId)
+                return false;
+            
+            actor.TeleportTo(target.Location);
+            return true;
         }
+
+        if (Params?.Length != 3) return false;
+        
+        ushort.TryParse(Params[0].ToString(), out var x);
+        ushort.TryParse(Params[1].ToString(), out var y);
+        byte.TryParse(Params[2].ToString(), out var z);
+
+        actor.TeleportTo(x, y, z);
 
         return true;
     }
