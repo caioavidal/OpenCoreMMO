4f9770e368b02a3ac474469cf6487d04ec377282
caioavidal <caio.avidal@gmail.com>
2023-06-01 17:55:41 -0300

    refact

--- tests/NeoServer.Game.Tests/Helpers/TestSemaphore.cs
+++ tests/NeoServer.Game.Tests/Helpers/TestSemaphore.cs
@@ -2,7 +2,7 @@ using System.Threading;
 
 namespace NeoServer.Game.Tests.Helpers;
 
-public class TestSemaphore
+public static class TestSemaphore
 {
-    public static SemaphoreSlim Semaphore = new SemaphoreSlim(2); // Set the initial count to 2
+    public static readonly SemaphoreSlim Semaphore = new(2); // Set the initial count to 2
 }
