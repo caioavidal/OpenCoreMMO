33ea6d3a1396b103efbf2e7ee07091f6e9dcb970
caioavidal <caio.avidal@gmail.com>
2023-07-04 16:17:02 -0300

    code refactor

--- src/Loaders/NeoServer.Loaders/Monsters/Converters/MonsterResistanceConverter.cs
+++ src/Loaders/NeoServer.Loaders/Monsters/Converters/MonsterResistanceConverter.cs
@@ -14,55 +14,26 @@ public static class MonsterResistanceConverter
 
         foreach (var element in data.Elements)
         {
-            var immunity = DamageType.None;
-            
-            if (element.Key.Contains("energy", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Energy;
-            else if (element.Key.Contains("holy", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Holy;
-            else if (element.Key.Contains("earth", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Earth;
-            else if (element.Key.Contains("death", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Death;
-            else if (element.Key.Contains("fire", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Fire;
-            else if (element.Key.Contains("ice", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Ice;
-            else if (element.Key.Contains("drown", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Drown;
-            else if (element.Key.Contains("lifeDrain", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.LifeDrain;
-            else if (element.Key.Contains("manaDrain", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.ManaDrain;
-            else if (element.Key.Contains("firePercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Fire;
-            else if (element.Key.Contains("physical", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Physical;
-            else if (element.Key.Contains("icePercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Ice;
-            else if (element.Key.Contains("earthPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Earth;
-            else if (element.Key.Contains("energyPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Energy;
-            else if (element.Key.Contains("holyPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Holy;
-            else if (element.Key.Contains("deathPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Death;
-            else if (element.Key.Contains("drownPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Drown;
-            else if (element.Key.Contains("lifeDrainPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.LifeDrain;
-            else if (element.Key.Contains("manaDrainPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.ManaDrain;
-            else if (element.Key.Contains("physicalPercent", StringComparison.InvariantCultureIgnoreCase))
-                immunity = DamageType.Physical;
-            
+            var immunity = element.Key.ToLowerInvariant() switch
+            {
+                var key when key.Contains("energy") || key.Contains("energypercentage") => DamageType.Energy,
+                var key when key.Contains("holy") || key.Contains("holypercentage") => DamageType.Holy,
+                var key when key.Contains("earth") || key.Contains("earthpercentage") => DamageType.Earth,
+                var key when key.Contains("death") || key.Contains("deathpercentage") => DamageType.Death,
+                var key when key.Contains("fire") || key.Contains("firepercent") => DamageType.Fire,
+                var key when key.Contains("ice") || key.Contains("icepercent") => DamageType.Ice,
+                var key when key.Contains("drown") || key.Contains("drownpercent") => DamageType.Drown,
+                var key when key.Contains("lifedrain") || key.Contains("lifedrainpercent") => DamageType.LifeDrain,
+                var key when key.Contains("manadrain") || key.Contains("manadrainpercent") => DamageType.ManaDrain,
+                var key when key.Contains("physical") || key.Contains("physicalpercent") => DamageType.Physical,
+                _ => DamageType.None
+            };
 
-            if (immunity is DamageType.None)
+            if (immunity == DamageType.None)
             {
                 Console.WriteLine($"{element.Key} not handled for monster {data.Name}");
             }
-            
+
             immunity = immunity == DamageType.None ? DamageType.Melee : immunity;
 
             immunities.TryAdd(immunity, element.Value);
