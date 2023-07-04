f0f301aed7918b61bb3391f12f9b1230440924dc
caioavidal <caio.avidal@gmail.com>
2023-07-04 16:08:00 -0300

    Add non handled immunities and flags

--- src/Loaders/NeoServer.Loaders/Monsters/Converters/MonsterResistanceConverter.cs
+++ src/Loaders/NeoServer.Loaders/Monsters/Converters/MonsterResistanceConverter.cs
@@ -4,7 +4,7 @@ using NeoServer.Game.Common.Item;
 
 namespace NeoServer.Loaders.Monsters.Converters;
 
-public class MonsterResistanceConverter
+public static class MonsterResistanceConverter
 {
     public static IDictionary<DamageType, sbyte> Convert(MonsterData data)
     {
@@ -14,7 +14,8 @@ public class MonsterResistanceConverter
 
         foreach (var element in data.Elements)
         {
-            var immunity = DamageType.Melee;
+            var immunity = DamageType.None;
+            
             if (element.Key.Contains("energy", StringComparison.InvariantCultureIgnoreCase))
                 immunity = DamageType.Energy;
             else if (element.Key.Contains("holy", StringComparison.InvariantCultureIgnoreCase))
@@ -27,8 +28,44 @@ public class MonsterResistanceConverter
                 immunity = DamageType.Fire;
             else if (element.Key.Contains("ice", StringComparison.InvariantCultureIgnoreCase))
                 immunity = DamageType.Ice;
+            else if (element.Key.Contains("drown", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Drown;
+            else if (element.Key.Contains("lifeDrain", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.LifeDrain;
+            else if (element.Key.Contains("manaDrain", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.ManaDrain;
+            else if (element.Key.Contains("firePercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Fire;
+            else if (element.Key.Contains("physical", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Physical;
+            else if (element.Key.Contains("icePercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Ice;
+            else if (element.Key.Contains("earthPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Earth;
+            else if (element.Key.Contains("energyPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Energy;
+            else if (element.Key.Contains("holyPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Holy;
+            else if (element.Key.Contains("deathPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Death;
+            else if (element.Key.Contains("drownPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Drown;
+            else if (element.Key.Contains("lifeDrainPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.LifeDrain;
+            else if (element.Key.Contains("manaDrainPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.ManaDrain;
+            else if (element.Key.Contains("physicalPercent", StringComparison.InvariantCultureIgnoreCase))
+                immunity = DamageType.Physical;
+            
 
-            immunities.Add(immunity, element.Value);
+            if (immunity is DamageType.None)
+            {
+                Console.WriteLine($"{element.Key} not handled for monster {data.Name}");
+            }
+            
+            immunity = immunity == DamageType.None ? DamageType.Melee : immunity;
+
+            immunities.TryAdd(immunity, element.Value);
         }
 
         return immunities;
