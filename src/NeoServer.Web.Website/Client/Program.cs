cf8f1eb91488701c9f79bc7861723ecf975853f2
sylviot <sylvio.tavares@hotmail.com>
2023-10-28 16:35:46 -0400

    initialize website project

---
+++ src/NeoServer.Web.Website/Client/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NeoServer.Web.Website;
+
+namespace NeoServer.Web.Website
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
